bddaff54d30dd0dacd1cdb1b6e11d8ea46c116b4
unknown <mwananjelaelisha36@gmail.com>
2026-08-25 19:23:12 +0300

    my  commit and build  for ptoduction

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,20 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-  ],
-  server:{
-    port:3000,
-    open:true,
-  }
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: 3000,
+    open: true,
+  },
+  // Add this for Vercel deployment
+  base: '/',
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
